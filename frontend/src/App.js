ae9bf33ef07fc950558b86cf982517805a233ca3
Andres Gomez <info@andresfgomez.com>
2020-10-14 17:05:37 -0400

    Backend code uploaded + some changes in the front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Container>
           <main className="py-3">
             <Route path="/" component={HomeScreen} exact />
+            <Route path="/products" component={HomeScreen} exact />
             <Route path="/product/:id" component={ProductScreen} />
           </main>
         </Container>
